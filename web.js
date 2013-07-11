2f6c050a163588737abc620cd3fdb2f71b41f852
paulwhittemore <paulwhittemore@comcast.net>
2013-07-11 01:56:57 +0000

    Read string from file.

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var hello_buf = fs.readFileSync('index.html');
+var hello_str = hello_buf.toString();
+
+//console.log(hello_str);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(hello_str);
 });
 
 var port = process.env.PORT || 5000;
