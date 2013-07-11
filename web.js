014ebb4f814ff1447413e6f57f4ffcb51333e763
paulwhittemore <paulwhittemore@comcast.net>
2013-07-11 01:27:31 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var hello_buf = fs.readfile('index.html', function (err, data) {
+    if (err) throw err;
+    return data;
+});
+
+var hello_str = hello_buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(hello_str);
 });
 
 var port = process.env.PORT || 5000;
