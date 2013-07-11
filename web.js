0ce036792eed907ff79061fd99db9c23bf80f41b
paulwhittemore <paulwhittemore@comcast.net>
2013-07-11 01:44:39 +0000

    Revert "Updated web.js"
    This reverts commit 014ebb4f814ff1447413e6f57f4ffcb51333e763.

--- web.js
+++ web.js
@@ -2,15 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var hello_buf = fs.readfile('index.html', function (err, data) {
-    if (err) throw err;
-    return data;
-});
-
-var hello_str = hello_buf.toString();
-
 app.get('/', function(request, response) {
-  response.send(hello_str);
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
